aa1bfcba6fdb6c40a72eb65753492442b60fc851
Huynh-Duy-Quy <huynhduyquy2001@gmail.com>
2023-05-30 15:00:47 +0700

    alo

---
+++ demo_slide4/src/main/java/com/poly/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.poly.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+@Autowired
+HttpServletRequest request;
+
+@GetMapping("/home/test")
+public String homeTest() {
+	request.setAttribute("message", "Thông điệp gửi qua View");
+	return "home/test";
+}
+}
